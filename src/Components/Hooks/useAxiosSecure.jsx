41a8abe70834b682d95207c5fe318e1eab384ae1
Th0u305 <ahnaf4411@gmail.com>
2025-01-19 09:15:47 +0600

    admin dshboard

---
+++ src/Components/Hooks/useAxiosSecure.jsx
@@ -0,0 +1,62 @@
+import axios from "axios";
+import { useNavigate } from "react-router";
+import { useContext } from "react";
+import { AuthContext } from "../Context/ContextProvider";
+
+const axiosSecure = axios.create({
+  baseURL: `${import.meta.env.VITE_API_URL}`,
+  withCredentials: true,
+});
+const useAxiosSecure = () => {
+  const navigate = useNavigate();
+  const { signOut } = useContext(AuthContext);
+
+
+  // If  localstorage used to store cookie 
+
+  // axiosSecure.interceptors.request.use(
+  //   function (config) {
+  //     const token = localStorage.getItem("access-token");
+  //     // console.log('request stopped by interceptors', token)
+  //     config.headers.authorization = `Bearer ${token}`;
+  //     return config;
+  //   },
+  //   function (error) {
+  //     // Do something with request error
+  //     return Promise.reject(error);
+  //   }
+  // );
+
+
+  // if cookie-parser used to store cookie
+
+  // request interceptor to add authorization header for every secure call to teh api
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      return config;
+    },
+    function (error) {
+      return Promise.reject(error);
+    }
+  );
+
+  // intercepts 401 and 403 status
+  axiosSecure.interceptors.response.use(
+    function (response) {
+      return response;
+    },
+    async (error) => {
+      const status = error.response.status;
+
+      if (status === 401 || status === 403) {
+        await logOut();
+        navigate("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+
+  return axiosSecure;
+};
+
+export default useAxiosSecure;
